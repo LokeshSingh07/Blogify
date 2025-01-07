6f0bd32c7b7edfd25371aaea17faf55b9100a1f7
Lokesh Singh <122585548+LokeshSingh07@users.noreply.github.com>
2025-01-07 21:31:07 +0530

    zod validation

---
+++ Common/src/index.ts
@@ -0,0 +1,39 @@
+import { z } from 'zod'
+
+
+// User Schema
+export const signupInput = z.object({
+    email: z.string().email(),
+    password: z.string().min(8),
+    name : z.string().optional()
+})
+
+export const signinInput = z.object({
+    email: z.string().email(),
+    password: z.string().min(8)
+});
+
+
+// blog Schema
+export const createBlog = z.object({
+    title: z.string().min(3).max(50),
+    description: z.string().min(10).max(500),
+    coverImage: z.string().optional(),
+})
+export const updateBlog = z.object({
+    id: z.string().uuid(),
+    title: z.string().min(3).max(50),
+    description: z.string().min(10).max(500),
+    coverImage: z.string().url(),
+    published: z.boolean().optional()
+})
+
+
+
+
+// type inference in zod
+type SignupInput = z.infer<typeof signupInput>
+type SigninInput = z.infer<typeof signinInput>
+
+type CreateBlog = z.infer<typeof createBlog>
+type UpdateBlog = z.infer<typeof updateBlog>
