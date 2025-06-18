c8c24926d15d80dd913f01b0aaa23b0ecbe523e2
Lokesh Singh <lokeshsingh0089@gmail.com>
2025-06-18 17:22:27 +0530

    linting fix

--- Frontend/src/pages/Publish.tsx
+++ Frontend/src/pages/Publish.tsx
@@ -1,4 +1,4 @@
-import React, { useEffect, useState } from "react"
+import React, { useState } from "react"
 import { AppBar } from "../components/AppBar"
 import { useNavigate } from "react-router-dom";
 import axios from "axios";
